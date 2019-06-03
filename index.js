8709935e90611bac7cbfada3d6a59a13f2717814
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-06-03 19:35:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function breakOut(array, changeValue, stopValue) {
+  array.forEach((element, index, array) => {
+    if(element===stopValue) {
+      break
+    }
+    array[index] = changeValue
+  });
+  return array
+}
